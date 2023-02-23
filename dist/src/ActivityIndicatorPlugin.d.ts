78d4908856809028936aa59ac1aafb2c49c48e77
Justin Kimbrell <jkimbrell@actengage.com>
2023-02-23 11:15:20 -0500

    Removed dist and updated gitignore

--- dist/src/ActivityIndicatorPlugin.d.ts
+++
@@ -1,9 +0,0 @@
-import { App } from 'vue';
-interface ActivityIndicatorPluginOptions {
-    key?: string;
-    indicators: {
-        [key: string]: any;
-    };
-}
-declare const _default: (app: App, options: ActivityIndicatorPluginOptions) => void;
-export default _default;
