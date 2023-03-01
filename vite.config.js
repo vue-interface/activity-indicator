4b63ff7eb1ba6726c3f3551f764fed90b399201f
Mitchell Solomon <msolomonqwerty@gmail.com>
2023-03-01 14:58:12 -0500

    vite config update

--- vite.config.js
+++ vite.config.js
@@ -3,25 +3,32 @@ import { pascalCase } from 'change-case';
 import path from 'path';
 import { defineConfig } from 'vite';
 import dts from 'vite-plugin-dts';
-import { name } from './package.json';
+import pkg from './package.json';
 
-const fileName = name.split('/')[1];
+const fileName = pkg.name.split('/')[1];
+
+const external = [
+    ...(pkg.dependencies ? Object.keys(pkg.dependencies) : []),
+    ...(pkg.peerDependencies ? Object.keys(pkg.peerDependencies) : [])
+];
+
 
 export default defineConfig({
     build: {
+        sourcemap: true,
         lib: {
             entry: path.resolve(__dirname, 'index.ts'),
             name: pascalCase(fileName),
             fileName,
         },
         rollupOptions: {
-            external: ['@vue-interface/component-registry', 'vue'],
-            output: {
-                globals: {
-                    '@vue-interface/component-registry': 'ComponentRegistry',
-                    'vue': 'Vue'
-                },
-            }
+            external,
+            // output: {
+            //     globals: {
+            //         '@vue-interface/component-registry': 'ComponentRegistry',
+            //         'vue': 'Vue'
+            //     },
+            // }
         },
         watch: !process.env.NODE_ENV && {
             include: [
